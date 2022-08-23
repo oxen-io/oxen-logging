f170becea6aa13e2cf6a18b96871b6d72104d13b
Jason Rhinelander <jason@imaginary.ca>
2022-08-23 20:35:38 -0300

    Add support for fmt::text_style
    This allows:
    
        log::info(cat, fg(fmt::color::red), "asdf {}", 42);
    
    to print in red foreground text (and any other fmt/color.h styles can be
    used).  This has three advantages over:
    
        log::info(cat, fmt::format(fg(fmt::color::red), "asdf {}", 42));
    
    1. Less verbose.
    2. Doesn't do formatting unless the log level is low enough to show the
       message (the version with the fmt::format version always formats).
    3. Doesn't double-format, if the fmt::format'ed value ends up having
       some '{}' format string in it.

--- example/example.cpp
+++ example/example.cpp
@@ -1,5 +1,5 @@
 #include <oxen/log.hpp>
-
+#include <fmt/color.h>
 
 int main() {
     using namespace oxen::log;
@@ -36,4 +36,17 @@ int main() {
     set_level(cat_bar, Level::debug);
     info(cat_foo, "hello {}", 42);
     info(cat_bar, "hello {}", 42);
+
+    info(cat_bar, fg(fmt::color::green), "green!");
+    info(cat_bar, fg(fmt::color::red), "red!");
+    critical(
+            cat_foo,
+            fg(fmt::color::black) | bg(fmt::color::yellow) | fmt::emphasis::bold |
+                    fmt::emphasis::underline | fmt::emphasis::italic,
+            "BLACK");
+    error(cat_foo,
+          fg(fmt::color::white) | bg(fmt::color::red) | fmt::emphasis::bold |
+                  fmt::emphasis::underline | fmt::emphasis::italic,
+          "WHITE {}",
+          42);
 }
