d344630a8bbea9a2966902baca42b6242d7673f6
Jason Rhinelander <jason@imaginary.ca>
2022-12-16 13:22:46 -0400

    Support a list of source root prefixes
    Some code bases have code in multiple places (e.g. src/ and include/)
    but we have no way to strip them so would have to strip the project root
    and have the `src/` or `include/` prefixed on everything.
    
    This commit allows a list of prefixes and attempts to remove them all.
    It also tweaks the way "../" and "/" get removed which ought to make it
    slightly more robust against weird paths.

--- include/oxen/log/internal.hpp
+++ include/oxen/log/internal.hpp
@@ -19,22 +19,27 @@ spdlog::sink_ptr make_sink(Type type, const std::string& file);
 
 bool is_ansicolor_sink(const spdlog::sink_ptr& sink);
 
-template <typename... More>
-inline static constexpr std::string_view strip_prefixes(
-        std::string_view str, std::string_view prefix, More&&... more_prefixes) {
-    if (str.substr(0, prefix.size()) == prefix)
-        str.remove_prefix(prefix.size());
-    if constexpr (sizeof...(More) == 0)
-        return str;
-    else
-        return strip_prefixes(str, std::forward<More>(more_prefixes)...);
-}
+#ifndef OXEN_LOGGING_SOURCE_ROOTS_LEN
+#define OXEN_LOGGING_SOURCE_ROOTS_LEN 0
+#endif
+
+inline constexpr std::array<std::string_view, OXEN_LOGGING_SOURCE_ROOTS_LEN> source_prefixes = {
+#ifdef OXEN_LOGGING_SOURCE_ROOTS
+        OXEN_LOGGING_SOURCE_ROOTS,
+#endif
+};
 
 inline auto spdlog_sloc(const slns::source_location& loc) {
     std::string_view filename{loc.file_name()};
-#ifdef OXEN_LOGGING_SOURCE_ROOT
-    filename = strip_prefixes(filename, OXEN_LOGGING_SOURCE_ROOT, "..", "/");
-#endif
+    for (const auto& prefix : source_prefixes) {
+        if (filename.substr(0, prefix.size()) == prefix) {
+            filename.remove_prefix(prefix.size());
+            if (!filename.empty() && filename[0] == '/')
+                filename.remove_prefix(1);
+        }
+    }
+    while (filename.substr(0, 3) == "../")
+        filename.remove_prefix(3);
 
     return spdlog::source_loc{filename.data(), static_cast<int>(loc.line()), loc.function_name()};
 }
